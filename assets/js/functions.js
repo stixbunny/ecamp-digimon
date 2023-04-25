0c96cb2435794a3cf260439c593962ace0d656f1
Milenko Castillo <milenko.castillo@gmail.com>
2023-04-25 14:36:08 -0400

    style mostly done, missing single digimon fetch

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,10 +1,23 @@
 const select = document.getElementById("digimon-select");
-
+const digimonCard = document.getElementById("digimon-card");
+fillDigimonSelect();
 
 function fillDigimonSelect() {
-
+    fetch("https://digimon-api.vercel.app/api/digimon")
+    .then(response => response.json())
+    .then(digimonList => {
+        for (let digimon of digimonList) {
+            console.log(digimon);
+            let option = document.createElement("option");
+            option.value = digimon.name;
+            option.innerHTML = digimon.name;
+            select.appendChild(option);
+        }
+    })
 }
 
 function getDigimon(select) {
-
+    if(digimonCard.classList.contains("d-none")) {
+        digimonCard.classList.remove("d-none");
+    }
 }

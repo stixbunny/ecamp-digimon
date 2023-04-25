b1ecebb4ec4ca3bb3f43ffcd9e0392b09f09332f
Milenko Castillo <milenko.castillo@gmail.com>
2023-04-25 19:21:06 -0400

    added card display, searchbar and modal alert

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,23 +1,71 @@
 const select = document.getElementById("digimon-select");
 const digimonCard = document.getElementById("digimon-card");
 fillDigimonSelect();
+let errorModal = new bootstrap.Modal(document.getElementById('error-modal'));
+
 
 function fillDigimonSelect() {
     fetch("https://digimon-api.vercel.app/api/digimon")
-    .then(response => response.json())
-    .then(digimonList => {
-        for (let digimon of digimonList) {
-            console.log(digimon);
-            let option = document.createElement("option");
-            option.value = digimon.name;
-            option.innerHTML = digimon.name;
-            select.appendChild(option);
-        }
-    })
+        .then(response => response.json())
+        .then(digimonList => {
+            for (let digimon of digimonList) {
+                let option = document.createElement("option");
+                option.value = digimon.name;
+                option.innerHTML = digimon.name;
+                select.appendChild(option);
+            }
+        })
 }
 
 function getDigimon(select) {
-    if(digimonCard.classList.contains("d-none")) {
-        digimonCard.classList.remove("d-none");
+    const digimonName = select.value;
+    fetch(`https://digimon-api.vercel.app/api/digimon/name/${digimonName}`)
+        .then(response => response.json())
+        .then(digimonList => {
+            let digimon = digimonList[0];
+            let digimonNameDiv = document.createElement("div");
+            let digimonImg = document.createElement("img");
+            let digimonLvlDiv = document.createElement("div");
+            digimonNameDiv.className = "card-header";
+            digimonNameDiv.innerHTML = digimon.name;
+            digimonImg.src = digimon.img;
+            digimonImg.className = "card-img";
+            digimonLvlDiv.className = "card-footer";
+            digimonLvlDiv.innerHTML = `Nivel: ${digimon.level}`;
+            digimonCard.replaceChildren(digimonNameDiv, digimonImg, digimonLvlDiv);
+            if(digimonCard.classList.contains("d-none")) {
+                digimonCard.classList.remove("d-none");
+            }
+        })
+    
+}
+
+function findDigimon(form) {
+    const digimonName = form["digimon"].value;
+    if(digimonName === "") {
+        return false;
     }
+    fetch(`https://digimon-api.vercel.app/api/digimon/name/${digimonName}`)
+        .then(response => response.json())
+        .then(digimonList => {
+            let digimon = digimonList[0];
+            let digimonNameDiv = document.createElement("div");
+            let digimonImg = document.createElement("img");
+            let digimonLvlDiv = document.createElement("div");
+            digimonNameDiv.className = "card-header";
+            digimonNameDiv.innerHTML = digimon.name;
+            digimonImg.src = digimon.img;
+            digimonImg.className = "card-img";
+            digimonLvlDiv.className = "card-footer";
+            digimonLvlDiv.innerHTML = `Nivel: ${digimon.level}`;
+            digimonCard.replaceChildren(digimonNameDiv, digimonImg, digimonLvlDiv);
+            if(digimonCard.classList.contains("d-none")) {
+                digimonCard.classList.remove("d-none");
+            }
+        })
+        .catch(function(error) {
+            errorModal.toggle();
+            return false;
+        });
+    return false;
 }
